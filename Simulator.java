f46a5a653a437d762e7052c81bb4663fac3d6364
ned-park <87483870+ned-park@users.noreply.github.com>
2021-12-28 13:37:58 -0500

    Added an interface for creating Simulators

---
+++ Simulator.java
@@ -0,0 +1,33 @@
+package dla;
+
+/**
+ * @author Ned Park
+ *
+ * An interface for all types of DLA Simulators
+ */
+public interface Simulator {
+
+	
+	/**
+	 * Prints the current state of the simulator to the console 
+	 */
+	void print();
+	
+	
+	/**
+	 * Inserts a particle into the system, limited by the the number of tries in order
+	 * to avoid infinite loops in dense systems.
+	 * 
+	 * @param tries the number of attempts to make when adding a particle to the system 
+	 */
+	void insertParticle(int tries);
+	
+	
+	/**
+	 * Standard toString() for a Simulator
+	 * 
+	 * @return A String containing the current state of the simulator
+	 */
+	String toString();
+	
+}
