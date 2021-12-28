ed828f6da3ebe9e758b3301b724bd1c74cc5f901
ned-park <87483870+ned-park@users.noreply.github.com>
2021-12-28 13:41:08 -0500

    Added a runner class for testing, and running thr program

---
+++ DLArunner.java
@@ -0,0 +1,70 @@
+package dla;
+
+/**
+ * @author Ned Park
+ *
+ *  Runner class for DLA simulators
+ */
+public class DLArunner {
+
+	public static void main(String[] args) {
+		int particles = 0;
+		char simType = 'c';
+		int attempts = 100000;
+		
+		for ( int i = 1; i < args.length; i+=2) {
+			if (args[i].equals("-t")) {
+				simType = args[i+1].charAt(0);
+			} else if (args[i].equals("-n")) {
+				try {
+					particles = Integer.parseInt(args[i+1]);
+				} catch (Exception e) {
+					printUsage(args[0]);
+				}
+			}  else if (args[i].equals("-a")) {
+				try {
+					attempts = Integer.parseInt(args[i+1]);
+				} catch (Exception e) {
+					printUsage(args[0]);
+				}
+			} else {
+				printUsage(args[0]);
+			}
+		}
+		
+		Simulator simulation;
+		
+		if (simType == 'o') {
+			simulation = new OpenSimulator();
+		} else {
+			simulation = new ClosedSimulator();
+		}		
+		
+		for (int p = 0; p < particles; p++) {
+			simulation.insertParticle(attempts);
+		}
+		
+		simulation.print();
+		
+		System.exit(0);
+		
+	}
+	
+	/**
+	 * Print the usage information for command line invocation
+	 * 
+	 * @param progName The program's name (in general given by args[0])
+	 */
+	public static void printUsage(String progName) {
+		System.out.println("Usage: " + progName + " -t <o|p> -n <int>");
+        System.out.println("-t the type of simulation, open (o) or closed (c)");
+        System.out.println("-n the number of particles to add to the system\n");
+        System.out.println("-a the number of attempts to make when adding an indivual particle\n"
+        		+ "to the system (defaults to 100,000 to prevent infinite indefinite hangs if n is too large)\n");
+        System.out.println("Examples:");
+        System.out.println(progName + " -t c -n 600");
+        System.out.println(progName + " -t o -n 700");
+        System.exit(0);
+	}
+
+}
